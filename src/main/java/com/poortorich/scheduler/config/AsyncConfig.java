16ecf3288ada8468592b34cab9a16cad9ebdfdeb
sseen2 <kse10023@naver.com>
2026-04-27 20:29:44 +0900

    chore: application.yml 분리 및 값 추가

--- src/main/java/com/poortorich/scheduler/config/AsyncConfig.java
+++ src/main/java/com/poortorich/scheduler/config/AsyncConfig.java
@@ -1,5 +1,6 @@
 package com.poortorich.scheduler.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
@@ -8,12 +9,21 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 public class AsyncConfig {
 
+    @Value("${ranking.task-executor.core-pool-size}")
+    private int corePoolSize;
+
+    @Value("${ranking.task-executor.max-pool-size}")
+    private int maxPoolSize;
+
+    @Value("${ranking.task-executor.queue-capacity}")
+    private int queueCapacity;
+
     @Bean(name = "rankingTaskExecutor")
     public TaskExecutor rankingTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(10);
-        executor.setQueueCapacity(25);
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
         executor.setThreadNamePrefix("ranking-batch-");
         executor.initialize();
         return executor;
