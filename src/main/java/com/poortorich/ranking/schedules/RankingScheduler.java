703238a6b7bdb0e7eecf1634ffc778265ad0f9f1
sseen2 <kse10023@naver.com>
2026-05-04 20:53:38 +0900

    feat: 배치 사이즈 변수 추가

--- src/main/java/com/poortorich/ranking/schedules/RankingScheduler.java
+++ src/main/java/com/poortorich/ranking/schedules/RankingScheduler.java
@@ -6,6 +6,7 @@ import com.poortorich.ranking.facade.RankingFacade;
 import com.poortorich.ranking.payload.response.RankingResponsePayload;
 import com.poortorich.websocket.stomp.command.subscribe.endpoint.SubscribeEndpoint;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -27,28 +28,37 @@ public class RankingScheduler {
     private final SimpMessagingTemplate messagingTemplate;
     private final ChatroomService chatroomService;
     private final TaskExecutor taskExecutor;
+    private final int batchSize;
 
     public RankingScheduler(
             RankingFacade rankingFacade,
             SimpMessagingTemplate messagingTemplate,
             ChatroomService chatroomService,
-            @Qualifier("rankingTaskExecutor") TaskExecutor taskExecutor
+            @Qualifier("rankingTaskExecutor") TaskExecutor taskExecutor,
+            @Value("${ranking.scheduler.batch-size}") int batchSize
     ) {
         this.rankingFacade = rankingFacade;
         this.messagingTemplate = messagingTemplate;
         this.chatroomService = chatroomService;
         this.taskExecutor = taskExecutor;
+        this.batchSize = batchSize;
     }
 
+    // 랭킹 집계 스케줄러
     @Scheduled(cron = "0 0 0 * * MON", zone = "Asia/Seoul")
     public void calculateAndBroadcastWeeklyRanking() {
+        // 랭킹 기능을 활성화한 운영 중인 채팅방 목록 조회
         List<Chatroom> activeChatrooms = chatroomService.getChatroomsByRankingEnabledIsTrue();
 
         AtomicInteger counter = new AtomicInteger();
+        // 조회한 채팅방 목록을 설정된 배치 크기 단위로 쪼개기
         Collection<List<Chatroom>> batches = activeChatrooms.stream()
-                .collect(Collectors.groupingBy(chatroom -> counter.getAndIncrement() / 100))
+                .collect(Collectors.groupingBy(chatroom -> counter.getAndIncrement() / batchSize))
                 .values();
 
+//        processBatch(activeChatrooms);
+
+//         하나의 배치를 하나의 스레드에서 비동기로 처리
         batches.forEach(batch -> {
             CompletableFuture.runAsync(() -> {
                 try {
@@ -62,9 +72,11 @@ public class RankingScheduler {
 
     private void processBatch(List<Chatroom> chatrooms) {
         chatrooms.forEach(chatroom -> {
+            // 랭킹 데이터 계산
             RankingResponsePayload payload = rankingFacade.calculateRanking(chatroom);
 
             if (!Objects.isNull(payload)) {
+                // 채팅방에 랭킹 데이터를 담아 메세지 전송
                 messagingTemplate.convertAndSend(
                         SubscribeEndpoint.CHATROOM_SUBSCRIBE_PREFIX + chatroom.getId(),
                         payload.mapToBasePayload()
